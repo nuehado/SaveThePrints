c780461ae77a7c890ae62c2206421003b7f45831
nuehado <donahue_philip@yahoo.com>
2020-01-14 00:08:33 -0500

    made quad floor cube prefab and run in editor grid snap script

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] [Range(10.0f, 20.0f)] private float gridSize;
+
+    void Update()
+    {
+        Vector3 snapPosition;
+        snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPosition.x, 0.0f, snapPosition.z);
+    }
+}
