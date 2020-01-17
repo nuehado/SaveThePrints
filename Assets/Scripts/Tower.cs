1ac57044dbf84beac5a00bff7971fd681711cc98
nuehado <donahue_philip@yahoo.com>
2020-01-16 22:29:22 -0500

    Added Turret and LookAt tower script

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AimAtEnemy();
+    }
+
+    private void AimAtEnemy()
+    {
+        Vector3 targetXZ = new Vector3(targetEnemy.position.x, objectToPan.position.y, targetEnemy.position.z);
+        objectToPan.LookAt(targetXZ);
+    }
+}
