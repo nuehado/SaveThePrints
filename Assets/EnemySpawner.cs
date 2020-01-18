e099e21329cc0bb946e7b889c4fc7e018b7405fa
nuehado <donahue_philip@yahoo.com>
2020-01-17 23:11:00 -0500

    Added basic Enemy Spawn script

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private float secondsBetweenSpawns = 1f;
+    private int enemyCount = 0;
+    [SerializeField] private int maxEnemies = 3;
+    [SerializeField] private GameObject enemy;
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy(0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator SpawnEnemy(float spawnDelay)
+    {
+        yield return new WaitForSeconds(spawnDelay);
+        Instantiate(enemy);
+        Debug.Log("enemy spawned at: " + Time.time);
+        enemyCount ++;
+        if (enemyCount < maxEnemies)
+        {
+            StartCoroutine(SpawnEnemy(secondsBetweenSpawns));
+        }
+        
+    }
+}
