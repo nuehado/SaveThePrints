b93760cd9e5f44991dbc3efe586293caf5803c26
nuehado <donahue_philip@yahoo.com>
2020-01-19 19:25:47 -0500

    added hit particle system and scripted activation in enemycollision.cs

--- Assets/Scripts/EnemyCollision.cs
+++ Assets/Scripts/EnemyCollision.cs
@@ -5,22 +5,80 @@ using UnityEngine;
 public class EnemyCollision : MonoBehaviour
 {
     [SerializeField] int hitPoints = 1;
-    [SerializeField] ParticleSystem hitEffect;
-    
+    private bool isBeingHit = false;
 
-    // Update is called once per frame
+    ParticleSystem[] childedParticles;
+
+    private float hitStoppedTimer = 0f;
+    [SerializeField] private float hittingStoppedTime = 0.1f;
+
+    private void Start()
+    {
+        childedParticles = GetComponentsInChildren<ParticleSystem>();
+    }
+
     void Update()
     {
         if (hitPoints <= 0)
         {
             KillEnemy();
         }
+
+        CheckForStoppedHitting();
+
+        if (isBeingHit == true)
+        {
+            PlayHitParticles();
+        }
+            
+        else if (isBeingHit == false)
+        {
+            StopHitParticles();
+        }
+            
     }
 
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void CheckForStoppedHitting()
+    {
+        hitStoppedTimer = hitStoppedTimer + Time.deltaTime;
+        if (hitStoppedTimer > hittingStoppedTime)
+        {
+            isBeingHit = false;
+        }
+    }
+    private void StopHitParticles()
+    {
+        foreach (ParticleSystem hitParticle in childedParticles)
+        {
+            hitParticle.Stop();
+        }
+    }
+
+    private void PlayHitParticles()
+    {
+        foreach (ParticleSystem hitParticle in childedParticles)
+        {
+            if (hitParticle.isStopped)
+            {
+                hitParticle.Play();
+            }
+
+        }
+        
+        
+    }
+
+    
 
     private void OnParticleCollision(GameObject other)
     {
-
+        isBeingHit = true;
+        hitStoppedTimer = 0f;
         ProcessHit();
         
         //Debug.Log("HIT. hits left = " + hitPoints);
@@ -35,8 +93,5 @@ public class EnemyCollision : MonoBehaviour
         
     }
 
-    private void KillEnemy()
-    {
-        Destroy(gameObject);
-    }
+
 }
