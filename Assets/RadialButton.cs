c7c9594ed787774c7297b22bd53ff9e02259d9cd
nuehado <donahue_philip@yahoo.com>
2020-01-28 13:31:52 -0500

    Added drag spinning to radial button in Main Menu scene

---
+++ Assets/RadialButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialButton : MonoBehaviour
+{
+    private Vector3 mouseOffset;
+    private float mouseZCoordinate;
+    [SerializeField] private Transform dialCenter;
+
+    private void OnMouseDrag()
+    {
+        mouseZCoordinate = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mouseOffset =  GetMouseAsWorldPoint();
+        Vector3 mouseToCenterLine = mouseOffset - dialCenter.position;
+        Vector3 dialKnobToCenterLine = gameObject.transform.position - dialCenter.position;
+        float dialAngle = Vector3.SignedAngle(mouseToCenterLine, dialKnobToCenterLine, dialCenter.up);
+        
+        if (dialAngle >= 15f && dialAngle < 120f)
+        {
+            dialCenter.transform.RotateAround(dialCenter.position, dialCenter.up, -30f);
+        }
+        if (dialAngle <= -15f && dialAngle > -120f)
+        {
+            {
+                dialCenter.transform.RotateAround(dialCenter.position, dialCenter.up, 30f);
+            }
+        }
+        
+    }
+
+    private Vector3 GetMouseAsWorldPoint()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = mouseZCoordinate;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+}
