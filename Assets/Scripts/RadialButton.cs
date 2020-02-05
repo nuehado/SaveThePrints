35a0eb6f3e472ce9920dc20d472da64bf45a02bc
nuehado <donahue_philip@yahoo.com>
2020-02-04 23:48:44 -0500

    Added longer than 4 menu options and made it so that rotating the radial button scrolls through those options on the 4 line lcd screen

--- Assets/Scripts/RadialButton.cs
+++ Assets/Scripts/RadialButton.cs
@@ -13,6 +13,19 @@ public class RadialButton : MonoBehaviour
     private Button highlightedButton = null;
     private int highlightedButtonIndex = 0;
 
+    [SerializeField] private ScrollRect menuScroller;
+    private int scrollIncrements = 0;
+    private float scrollIncrementDistance;
+    private int menuPositionIndex = 0;
+
+    private void Start()
+    {
+        menuScroller.verticalNormalizedPosition = 1f;
+        scrollIncrements = menuButtons.Count - 4;
+        scrollIncrementDistance = 1f / scrollIncrements;
+
+    }
+
     private void OnMouseDrag()
     {
         mouseZCoordinate = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
@@ -23,33 +36,72 @@ public class RadialButton : MonoBehaviour
         
         if (dialAngle >= 15f && dialAngle < 120f)
         {
-            dialCenter.transform.RotateAround(dialCenter.position, dialCenter.up, -18f);
-            if ( highlightedButtonIndex >= menuButtons.Count -1)
-            {
-                highlightedButtonIndex = 0;
-            }
-            else
-            {
-                highlightedButtonIndex++;
-            }
-            
+            RotateDialTransformCounterClockwise();
+            ScrollMenuButtonSelectDown();
         }
+
         if (dialAngle <= -15f && dialAngle > -120f)
         {
-            dialCenter.transform.RotateAround(dialCenter.position, dialCenter.up, 18f);
-
-            if (highlightedButtonIndex <= 0)
-            {
-                highlightedButtonIndex = menuButtons.Count - 1;
-            }
-            else
-            {
-                highlightedButtonIndex--;
-            }
+            RotateDialTransformClockwise();
+            ScrollMenuButtonSelectUp();
         }
+
         highlightedButton = menuButtons[highlightedButtonIndex];
         highlightedButton.Select();
         Debug.Log("selected button " + highlightedButtonIndex);
+    }
+
+    private void RotateDialTransformClockwise()
+    {
+        dialCenter.transform.RotateAround(dialCenter.position, dialCenter.up, 18f);
+    }
+
+    private void RotateDialTransformCounterClockwise()
+    {
+        dialCenter.transform.RotateAround(dialCenter.position, dialCenter.up, -18f);
+    }
+    private void ScrollMenuButtonSelectDown()
+    {
+        if (highlightedButtonIndex >= menuButtons.Count - 1)
+        {
+            menuScroller.verticalNormalizedPosition = 0f;
+            return;
+        }
+        else
+        {
+            if (menuPositionIndex < 4)
+            {
+                menuPositionIndex++;
+            }
+
+            highlightedButtonIndex++;
+            if (highlightedButtonIndex >= 4 && menuPositionIndex == 4)
+            {
+                menuScroller.verticalNormalizedPosition -= scrollIncrementDistance;
+            }
+        }
+    }
+
+    private void ScrollMenuButtonSelectUp()
+    {
+        if (highlightedButtonIndex <= 0)
+        {
+            menuScroller.verticalNormalizedPosition = 1f;
+            return;
+        }
+        else
+        {
+            if (menuPositionIndex > 0)
+            {
+                menuPositionIndex--;
+            }
+
+            highlightedButtonIndex--;
+            if (highlightedButtonIndex <= menuButtons.Count - 4 && menuPositionIndex == 0)
+            {
+                menuScroller.verticalNormalizedPosition += scrollIncrementDistance;
+            }
+        }
     }
 
     private Vector3 GetMouseAsWorldPoint()
@@ -65,4 +117,6 @@ public class RadialButton : MonoBehaviour
     {
         highlightedButton.onClick.Invoke();
     }
+
+
 }
