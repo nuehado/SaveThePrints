cdc09c2a6d394cef857917fe2ef264c4b366b601
nuehado <donahue_philip@yahoo.com>
2020-02-07 00:55:29 -0500

    Drag scrolling of menu added but it is pixel and seemingly mouse drag speed dependant

--- Assets/ScrollControlLimiter.cs
+++ Assets/ScrollControlLimiter.cs
@@ -9,35 +9,59 @@ public class ScrollControlLimiter : MonoBehaviour, IBeginDragHandler, IDragHandl
     [SerializeField] private float minDragDistance = 1f;
     private Vector3 previousMousePosition;
     private float yDraggedDistance = 0f;
+    private ScrollRect scrollRect;
+    private float scrollStepDistance = 0f;
+    private float scrollVerticalNormalizedPosition = 1f;
+
+    private void Start()
+    {
+        scrollRect = GetComponent<ScrollRect>();
+        scrollStepDistance = 1f / (GetComponentsInChildren<Button>().Length - 4);
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
-        GetComponent<ScrollRect>().vertical = false;
+        scrollRect.vertical = false;
         previousMousePosition = Input.mousePosition;
+        scrollVerticalNormalizedPosition = scrollRect.verticalNormalizedPosition;
     }
 
     public void OnDrag(PointerEventData data)
     {
         Vector3 newMousePosition = Input.mousePosition;
         yDraggedDistance = newMousePosition.y - previousMousePosition.y;
-        previousMousePosition = newMousePosition;
+
         if (yDraggedDistance >= minDragDistance)
         {
-            //scroll the ScrollRect
-            Debug.Log("scroll the scrollrect up");
-            yDraggedDistance = 0f;
+            scrollVerticalNormalizedPosition += scrollStepDistance;
+            if (scrollVerticalNormalizedPosition >= 1f)
+            {
+                scrollVerticalNormalizedPosition = 1f;
+
+            }
+            UpdateScrollPosition();
         }
-        if (yDraggedDistance <= -minDragDistance)
+
+        else if (yDraggedDistance <= -minDragDistance)
         {
-            //scroll the ScrollRect
-            Debug.Log("scroll the scrollrect down");
-            yDraggedDistance = 0f;
+            scrollVerticalNormalizedPosition -= scrollStepDistance;
+            if (scrollVerticalNormalizedPosition <= 0f)
+            {
+                scrollVerticalNormalizedPosition = 0f;
+            }
+            UpdateScrollPosition();
         }
+        previousMousePosition = newMousePosition;
     }
 
+    private void UpdateScrollPosition()
+    {
+        scrollRect.verticalNormalizedPosition = scrollVerticalNormalizedPosition;
+        yDraggedDistance = 0f;
+    }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        GetComponent<ScrollRect>().vertical = true;
+        scrollRect.vertical = true;
         yDraggedDistance = 0f;
     }
 
